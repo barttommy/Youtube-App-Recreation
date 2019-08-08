4db6ac93e37155b739d321e2704fde81f0b05b37
Tommy Bart <tommybart8@gmail.com>
2019-08-08 13:38:15 -0500

    Cleaned file structure

--- Youtube/View/MenuBar.swift
+++ Youtube/View/MenuBar.swift
@@ -55,35 +55,3 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-class MenuCell: BaseCell {
-    let imageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "trending")?.withRenderingMode(.alwaysTemplate)
-        iv.tintColor = UIColor.rgb(red: 91, green: 14, blue: 13)
-        return iv
-    }()
-    
-    override var isHighlighted: Bool {
-        didSet {
-            imageView.tintColor = isHighlighted ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
-        }
-    }
-    
-    override var isSelected: Bool {
-        didSet {
-            imageView.tintColor = isSelected ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
-        }
-    }
-    
-    override func setupViews() {
-        super.setupViews()
-       
-        addSubview(imageView)
-        addConstraintsWithFormat(format: "H:[v0(28)]", view: imageView)
-        addConstraintsWithFormat(format: "V:[v0(28)]", view: imageView)
-        
-        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
-        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
-    }
-}
---
+++ Youtube/View/MenuCell.swift
@@ -0,0 +1,41 @@
+//
+//  MenuCell.swift
+//  Youtube
+//
+//  Created by Thomas Bart on 8/8/19.
+//  Copyright © 2019 Thomas Bart. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: BaseCell {
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "trending")?.withRenderingMode(.alwaysTemplate)
+        iv.tintColor = UIColor.rgb(red: 91, green: 14, blue: 13)
+        return iv
+    }()
+    
+    override var isHighlighted: Bool {
+        didSet {
+            imageView.tintColor = isHighlighted ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            imageView.tintColor = isSelected ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+        }
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(imageView)
+        addConstraintsWithFormat(format: "H:[v0(28)]", view: imageView)
+        addConstraintsWithFormat(format: "V:[v0(28)]", view: imageView)
+        
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+}
